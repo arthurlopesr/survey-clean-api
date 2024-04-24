f240f5d417f66c5f9c9edc86c01468814510aa4a
Arthur Lopes <arthurlopr12@gmail.com>
2024-04-24 19:34:57 -0300

    feat: add validator lib to validate EmailValidatoAdapter

--- tests/utils/email-validator-adapter.spec.ts
+++ tests/utils/email-validator-adapter.spec.ts
@@ -1,9 +1,23 @@
 import { EmailValidatorAdapter } from "@/utils/email-validator-adapter"
+import validator from "validator"
+
+jest.mock('validator', () => ({
+  isEmail (): boolean {
+    return true
+  }
+}))
 
 describe('EmailValidator Adapter', () => {
-  it('shuld return false if valiator returns false', () => {
+  it('should return false if valiator returns false', () => {
     const sut = new EmailValidatorAdapter()
-    const isValid = sut.isValid('invalid_email@mail.com')
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+    const isValid = sut.isValid('invalid_email')
     expect (isValid).toBe(false)
+  })
+
+  it('should return true if valiator returns true', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('valid_email@mail.com')
+    expect (isValid).toBe(true)
   })
 })
