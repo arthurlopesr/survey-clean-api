747ccbf77dc2fbe1b65c60b539f574e11b0bbe87
Arthur Lopes <arthurlopr12@gmail.com>
2024-04-25 20:09:30 -0300

    feat: ensure BcryptAdapter returns a hash on success

--- src/infra/criptography/bcryot-adapter.ts
+++ src/infra/criptography/bcryot-adapter.ts
@@ -5,6 +5,7 @@ export class BcryptAdapter implements Encrypter {
   constructor(private readonly salt: number) {}
 
   async encrypt(value: string): Promise<string> {
-    return await bcrypt.hash(value, this.salt);
+    const hash=  await bcrypt.hash(value, this.salt);
+    return hash;
   }
 }
--- tests/infra/criptography/bcrypt-adpter.spec.ts
+++ tests/infra/criptography/bcrypt-adpter.spec.ts
@@ -1,12 +1,25 @@
-import bcrypt from 'bcrypt'
+import bcrypt, { hash } from 'bcrypt'
 import { BcryptAdapter } from '@/infra/criptography/bcryot-adapter'
 
+jest.mock('bcrypt', () => ({
+  async hash(): Promise<string> {
+    return 'hash'
+  }
+}))
+
 describe('BCrypt Adapter',() => {
-  it('Should call bcrypt with correct values', async () => {
+  it('should call bcrypt with correct values', async () => {
     const salt = 12
     const sut = new BcryptAdapter(salt)
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  it('should return a hash on success', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    const hash = await sut.encrypt('any_value')
+    expect(hash).toBe('hash')
   })
 })
