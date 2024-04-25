8e7a4a184a47edef1390eccbceb56d00d0f90f5a
Arthur Lopes <arthurlopr12@gmail.com>
2024-04-24 21:23:41 -0300

    test: ensure DbAddAccount throws if encrypter throws

--- tests/data/usecases/db-add-account.spec.ts
+++ tests/data/usecases/db-add-account.spec.ts
@@ -83,4 +83,18 @@ describe('DbAddAccount Usecase', () => {
       password: 'hashed_password'
     });
   });
+
+  it('should throw if AddAccontRepository throws', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut();
+    jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(
+      new Promise((resolve, reject) => reject(new Error())
+    ));
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    }
+    const promise = sut.add(accountData);
+    await expect(promise).rejects.toThrow();
+  });
 });
