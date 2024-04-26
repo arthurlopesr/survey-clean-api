3d488f9fee689f22ac2e5b430b7da27e9dcefae6
Arthur Lopes <arthurlopr12@gmail.com>
2024-04-26 15:57:17 -0300

    refactor: move map for mongo helper

--- src/infra/database/mongodb/repositories/account/account-repository.ts
+++ src/infra/database/mongodb/repositories/account/account-repository.ts
@@ -1,9 +1,10 @@
 import { AddAccountRepository } from '@/data/protocols';
+import { AccountModel } from '@/domain/models';
 import { AddAccountModel } from '@/domain/usecases';
 import { MongoHelper } from '@/infra/database/mongodb/helpers/mongo.helper'
 
 export class AccountMongoRepository implements AddAccountRepository {
-  async add(accountData: AddAccountModel): Promise<any> {
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
     const account = {
@@ -12,7 +13,6 @@ export class AccountMongoRepository implements AddAccountRepository {
       email: accountData.email,
       password: accountData.password
     }
-
     return account
   }
 }
