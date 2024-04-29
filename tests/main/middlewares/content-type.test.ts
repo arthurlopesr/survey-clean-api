6a3ca42840d6485c71d18a790a236569bfe3e11c
Arthur Lopes <arthurlopr12@gmail.com>
2024-04-29 14:09:57 -0300

    feat: ensure requests return json as default content type

---
+++ tests/main/middlewares/content-type.test.ts
@@ -0,0 +1,22 @@
+import request from 'supertest'
+import app from '@/main/config/app'
+
+describe('Content Type Middleware', () => {
+  test('should return default Content Type as json', async () => {
+    app.get('/test_content_type', (req, res) => {
+      res.send('')
+    })
+    await request(app)
+      .get('/test_content_type')
+      .expect('content-type', /json/)
+  })
+  test('should return XML Content Type when forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
+  })
+})
