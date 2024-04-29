a5137988231a4e0358c25a1a5da847d288bc4929
Arthur Lopes <arthurlopr12@gmail.com>
2024-04-29 14:41:54 -0300

    feat: add signup routes

---
+++ tests/main/routes/signup-routes.test.ts
@@ -0,0 +1,16 @@
+import request from 'supertest'
+import app from '@/main/config/app'
+
+describe('Signup Routes', () => {
+  test('should return an account on success', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      })
+      .expect(200)
+  })
+})
