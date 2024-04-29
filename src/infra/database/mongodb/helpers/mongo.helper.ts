46ba54a132631fdba225708ee816aa3d2778f019
Arthur Lopes <arthurlopr12@gmail.com>
2024-04-29 13:35:53 -0300

    feat: add express

--- src/infra/database/mongodb/helpers/mongo.helper.ts
+++ src/infra/database/mongodb/helpers/mongo.helper.ts
@@ -18,5 +18,5 @@ export const MongoHelper = {
   map(collection: any): any {
     const { _id, ...collectionWithoutId } = collection
     return Object.assign({}, collectionWithoutId, { id: _id })
-    },
   }
+}
